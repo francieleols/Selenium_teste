3fcb4a0e21153931838b7dae460297ada1ff8974
francieleols <francieleoliveiradasilva520@gmail.com>
2024-12-19 19:59:01 +0000

    segundo dia de construcao

--- main.py
+++ main.py
@@ -17,21 +17,54 @@ from bs4 import BeautifulSoup
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install())) 
 # iniciando navegador 
 
-#funçoes para abrir excel e colar dados
-def abrir_arquivo():
- arquivo = load_workbook('GOV BA Limite cartão.xlsx')
- planilha= arquivo.active
- return planilha
+#funçoes para abrir excel e ter acesso as cpf's da coluna A
+arquivo = load_workbook('GOV BA Limite cartão.xlsx') 
+planilha = arquivo.active 
 
-def colar_dados(planilha):
-  
-  dados = pyperclip.paste()
-  linhas = dados.split("\n")
-  for i, linha in enumerate(linhas):
-     valores = linha.split("\t")
-     for j, valor in enumerate(valores):
-            planilha.cell(row=i+1, column=j+1).value = valor
+coluna= 'A'
+valores_coluna = []
 
-#abrindo site
-driver.get("(https://www.saopauloconsig.org.br/home?0&3+/%27)")
+for cell in planilha[coluna][1:]: 
+    valores_coluna.append(cell.value)
+
+#lista de login e senha 
+
+login = [
+   "francielesilva"
+]
+senha= [
+   "Sf070124@"
+]
+
+
+#abrindo site 
+driver.get("(https://autenticacao.bancomaster.com.br/login)")
 time.sleep(5)
+
+Login= driver.find_element (By.XPATH,'/html/body/app-root/app-login/div/div[2]/mat-card/mat-card-content/form/div[1]/mat-form-field/div/div[1]/div[3]')
+Login.send_keys(login)
+
+Senha = driver.find_element (By.XPATH,'//*[@id="mat-input-1"]')
+Senha.send_keys(senha)
+time.sleep(5)
+
+driver.get("(https://autenticacao.bancomaster.com.br/)")
+time.sleep(5)
+
+# lista cpf para cadastro em plataforma
+
+for dado in valores_coluna:
+    campo = driver.find_element(By.ID, "campo")  
+    campo.send_keys(dado)
+
+    driver.get(dir_forms)
+time.sleep(1)
+num_cpf = 480  # Limitador de consultas
+contador = 0
+
+for index, row in dt_total.iterrows():
+    contador += 1
+    if num_cpf == contador:
+        print("TROCAR DE USUÁRIO")
+        driver.quit()
+        break
